5a12ee7aa9f78d8a551c546c1dc2c117753c8375
Svaym <162036192+Svaym@users.noreply.github.com>
2024-03-12 10:19:45 +0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    fontFamily:{
+      barlow: ['Barlow Semi Condensed', 'sens-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
